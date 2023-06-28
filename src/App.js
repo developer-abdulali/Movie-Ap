e29b8a5d4147822cfb4199badef5d35998ffee69
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-28 19:00:10 +0500

    added changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { Switch } from 'react-router-dom';
+import Home from "../src/components/Home/Home";
+import Header from "../src/components/Header/Header";
+import Footer from "../src/components/Footer/Footer";
+import PageNotFound from "../src/components/PageNotFound/PageNotFound";
+import MovieDetail from "../src/components/MovieDetail/MovieDetail";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Header></Header>
+          <Route path="/" component={Home} />
+          <Route path="/movie/:imdbID" component={MovieDetail} />
+          <Route component={PageNotFound} />
+   
+        <Footer />
+      </Router>
     </div>
   );
 }
