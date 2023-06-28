4333cf71f5c47b44dc45b14d4fb016eef562322e
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-29 03:59:54 +0500

    add api function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,26 @@
-import React from "react"
+import React, { useEffect } from "react"
+import MovieListing from "../../components/MovieListing/MovieListing"
+import MovieApi from "../../common/api/MovieApi"
+import { ApiKey } from "../../common/api/MovieApiKey"
 
 const Home = () => {
+  const movieText = "Harry"
+  useEffect(()=>{
+   const fetchMovies = async () => {
+    const response = await MovieApi
+    .get(`?apiKey=${ApiKey}&s=${movieText}&type=movie`)
+    .catch((error)=>{
+      console.log("Error:", error)
+    })
+    console.log("THE response from API", response)
+  } 
+  fetchMovies()
+  },[])
   return (
-    <div>Home</div>
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
   )
 }
 
