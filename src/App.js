13e93eeb93498566c31afbef9d1aacc44825d48e
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-29 03:42:04 +0500

    adding footer css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../src/components/Home/Home";
 import Header from "../src/components/Header/Header";
 import Footer from "../src/components/Footer/Footer";
-import PageNotFound from "../src/components/PageNotFound/PageNotFound"
+import PageNotFound from "../src/components/PageNotFound/PageNotFound";
 import MovieDetail from "../src/components/MovieDetail/MovieDetail";
 import "./App.scss";
 
@@ -12,11 +12,13 @@ function App() {
     <div className="App">
       <Router>
         <Header></Header>
-        <Routes>
-          <Route path="/" exact Component={Home} />
-          <Route path="/movie/:imdbID" Component={MovieDetail} />
-          <Route Component={PageNotFound} />
-        </Routes>
+        <div className="container">
+          <Routes>
+            <Route path="/" exact Component={Home} />
+            <Route path="/movie/:imdbID" Component={MovieDetail} />
+            <Route Component={PageNotFound} />
+          </Routes>
+        </div>
         <Footer />
       </Router>
     </div>
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,12 @@
-import React from "react"
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>footer</div>
+    <div className="footer">
+      <div>Movie App</div>
+      <div>©2023, Movie, Inc. or its affiliates</div>
+    </div>
   )
 }
 
