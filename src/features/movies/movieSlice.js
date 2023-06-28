267c0f2124450a826fc3db57cf36e0e2349f329e
developer-abdulali <ab.ali.soomro@gmail.com>
2023-06-29 04:29:24 +0500

    add store and slice

---
+++ src/features/movies/movieSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    movies: []
+}
+const movieSlice = createSlice ({
+    name: "movies",
+    initialState,
+    reducers: {
+        addMovie: (state, {payload}) => {
+            state.movies = payload;
+        }
+    }
+})
+export const {addMovie} = movieSlice.actions;
+export const getAllMovies = (state) => state.movies.movies;
+export default movieSlice.reducer;
